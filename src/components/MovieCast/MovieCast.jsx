b80a2fee7182f448791769f9ce423562d253f342
MaxKov86 <max.kov86@ukr.net>
2024-03-09 00:58:51 +0200

    Update MovieCast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -26,11 +26,11 @@ const MovieCast = () => {
           {actors.map(actor => (
             <li key={actor.id}>
               <img
-                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                src={`https://image.tmdb.org/t/p/w300${actor.profile_path}`}
                 alt={actor.name}
               />
-              <p>Actor:{actor.name}</p>
-              <p>Chapter:</p>
+              <p>Actor: {actor.name}</p>
+              <p>Character: {actor.character}</p>
             </li>
           ))}
         </ul>
